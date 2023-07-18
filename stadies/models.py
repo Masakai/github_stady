cff98814f32550071626bfac42ef40203b2b0623
masanorisakai <masanorisakai@icloud.com>
2023-07-18 14:48:58 +0900

    Add Book model to stadies app
    A new model, Book, has been added to the stadies application. This model includes the attributes title and author, both represented as CharFields. This addition will help manage book-related data throughout the application.

--- stadies/models.py
+++ stadies/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Book(models.Model):
+    """
+     Represents a book.
+
+    Attributes:
+        title (str): The title of the book.
+        author (str): The author of the book.
+    """
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+
+    # コメント
